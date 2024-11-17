49fce6d2b8e906a6d07a8c8096343fcd0015d426
HHlaingSwan <168323270+HHlaingSwan@users.noreply.github.com>
2024-11-17 14:50:51 +0630

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,55 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [expandItem, setExpandItem] = useState(3);
+  const sliding = [
+    { image: "/img-2.jpg" },
+    { image: "/img-4.jpg" },
+    { image: "/img-6.jpg" },
+    { image: "/img-1.jpg" },
+    { image: "/img-5.jpg" },
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    // { image: "/img-7.jpg" },
+  ]
+  return (
+    <>
+
+      <main className="w-screen h-screen bg-black overflow-hidden">
+        <div className="w-full h-full flex items-center justify-center transition-all duration-300 ease-in-out delay-100   relative p-4"
+          style={{
+            backgroundImage: `url(${sliding[expandItem].image})`,
+            backgroundSize: "cover",
+            backgroundRepeat: "no-repeat",
+            backgroundPosition: "center",
+          }}>
+          <div className="absolute top-0 left-0 w-full h-full inset-0 z-0 bg-black opacity-90 md:bg-[#12121280] md:backdrop:blur-3xl ">
+          </div>
+          <div className="max-w-7xl h-[50vh] flex justify-center items-center mt-10 gap-2 z-20 ">
+            {
+              sliding.map((item, index) => {
+                return (
+                  <div
+                    className={`h-full bg-white rounded-2xl cursor-pointer transition-all duration-300 delay-75 ease-in-out overflow-hidden ${expandItem === index ? "w-[40%]" : "w-[15%] hover:bg-gray-400"} min-w-[40px] block`}
+                    key={index}
+                    onClick={() => setExpandItem(index)}
+                  >
+                    <Image
+                      src={item.image}
+                      alt="image"
+                      width={3000}
+                      height={1500}
+                      className="object-cover w-full h-full"
+                    />
+                  </div>
+                );
+              })
+            }
+          </div>
         </div>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    </>
   );
 }
