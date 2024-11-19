7afcae3ddf4e8841e42a9fbf90dfdee6ac53bc4a
HHlaingSwan <168323270+HHlaingSwan@users.noreply.github.com>
2024-11-19 11:32:37 +0630

    change Responsive

--- src/app/page.js
+++ src/app/page.js
@@ -3,27 +3,25 @@ import Image from "next/image";
 import { useState } from "react";
 
 
+
 export default function Home() {
-  const [expandItem, setExpandItem] = useState(3);
+
+  const [expandItem, setExpandItem] = useState(2);
   const sliding = [
     { image: "/img-2.jpg" },
     { image: "/img-4.jpg" },
     { image: "/img-6.jpg" },
     { image: "/img-1.jpg" },
     { image: "/img-5.jpg" },
-
-    // { image: "/img-7.jpg" },
   ]
   return (
     <>
 
       <main className="w-screen h-screen bg-black overflow-hidden">
-        <div className="w-full h-full flex items-center justify-center transition-all duration-300 ease-in-out delay-100   relative p-4"
+        <div className="bgNormal bgResponsive w-full h-full flex items-center justify-center transition-all duration-300 ease-in-out delay-100   relative p-4"
           style={{
             backgroundImage: `url(${sliding[expandItem].image})`,
-            backgroundSize: "fill",
-            backgroundRepeat: "no-repeat",
-            backgroundPosition: "center",
+
           }}>
           <div className="absolute top-0 left-0 w-full h-full inset-0 z-0 bg-black opacity-50 ">
           </div>
